da273ad5452e773ca68912b944281d1c8be42f4e
kende42 <toth42kende@gmail.com>
2026-05-07 15:58:00 +0200

    pvp.py

--- pvp.py
+++ pvp.py
@@ -74,7 +74,7 @@ while running:
 
 							for i in range(p[0] - 1, p[0] + 2):
 								for j in range(p[1] - 1, p[1] + 2):
-									if i >= 0 and j >= 0 and i <= 6 and j <= 6:
+									if i >= 0 and j >= 0 and i < BOARD_SIZE and j < BOARD_SIZE:
 										if not (i == p[0] and j == p[1]):
 											if fields[(i, j)] == "red":
 												red_graph.addEdge((p[0], p[1]), (i, j))
@@ -97,7 +97,7 @@ while running:
 
 							for i in range(p[0] - 1, p[0] + 2):
 								for j in range(p[1] - 1, p[1] + 2):
-									if i >= 0 and j >= 0 and i <= 6 and j <= 6:
+									if i >= 0 and j >= 0 and i < BOARD_SIZE and j < BOARD_SIZE:
 										if not (i == p[0] and j == p[1]):
 											if fields[(i, j)] == "blue":
 												blue_graph.addEdge((p[0], p[1]), (i, j))
